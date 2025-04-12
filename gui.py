1cd31eb34c2ec55504b2c5e2e74ccede989c92c3
kakoikeisuke <k.kakoi0531@gmail.com>
2025-04-12 23:10:20 +0900

    PySide6 のインポートの確認

---
+++ gui.py
@@ -0,0 +1,4 @@
+import PySide6
+
+def gui_test():
+    print(PySide6.__version__)
---
+++ main.py
@@ -0,0 +1,8 @@
+import gui
+
+def main():
+    print('Hello, world!')
+
+if __name__ == '__main__':
+    main()
+    gui.gui_test()
